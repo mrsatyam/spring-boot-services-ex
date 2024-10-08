41a6e25493c0a5f55af6b848955faffb30a5f35d
Satyam <mrsatyamjoshi@gmail.com>
2024-10-08 22:20:55 +0530

    Implemented facebook OAuth authentication

--- src/main/java/com/example/finacleservice/controller/UserProfileController.java
+++ src/main/java/com/example/finacleservice/controller/UserProfileController.java
@@ -6,12 +6,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class UserProfileController {
 
     @GetMapping("/profile")
     public String userProfile(Model model, @AuthenticationPrincipal OAuth2User user) {
-        model.addAttribute("user", user.getAttributes());
+        Map<String, Object> attributes = new HashMap<>(user.getAttributes());
+        attributes.putIfAbsent("picture", "https://avatars.githubusercontent.com/u/33051420?v=4");
+        model.addAttribute("user", attributes);
         return "profile";
     }
 }
